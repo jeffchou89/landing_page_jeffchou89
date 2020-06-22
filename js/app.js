8dd775b6e7fcb9336d8c52de0eaa968035027101
unknown <jeffrey.chou.89@gmail.com>
2020-06-21 23:48:17 -0400

    complete app.js overhaul. active class can be added while scrolling and can be removed, but still no CSS changes on screen.

--- js/app.js
+++ js/app.js
@@ -1,62 +1,16 @@
-/**
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
-*/
-
 // Global Variables
-const sections = document.getElementsByTagName("section");
+const sections = document.querySelectorAll("section");
 const navbarList = document.querySelector("#navbar__list");
-console.log(navbarList);
 
-// build the nav-----------------------------------------------------
+// Build the nav-----------------------------------------------------
 function buildNavbar(sections) {
-    console.log("line 15");
-    console.log(sections);
     for(let section of sections) {
         let sectionID = section.getAttribute("id");
-        console.log("sectionID: " + sectionID);
-
         let navLinkName = section.getAttribute("data-nav");
-        console.log("navLinkName: " + navLinkName);
 
         const htmlLink = `<li><a href="#${sectionID}" class="menu__link">${navLinkName}</a></li>`;
         navbarList.insertAdjacentHTML("beforeend", htmlLink);
-    }
-}
-
-// Add class 'active' to section when near top of viewport
-function setSectionInViewActive(sections) {
-    for (const section of sections) {
-        const sectionID = section.getAttribute("id");
-        console.log("LINE 33 SECTION ID: " + sectionID);
-
-        const currentSection = document.querySelector(
-            `a[href="#${sectionID}"]`
-          );
-        
-        if (isInViewport(currentSection)) {
-            console.log("HERE: LINE 35");
-            let currentSection = document.getElementById(sectionID);
-            currentSection.classList.add("active");
-            currentSection.classList.add("menu__link--active");
-        } else {
-            currentSection.classlist.remove("active");
-            currentSection.classList.remove("menu__link--active");
-        }
-    }
-}
-
-// Check if section is in viewport
-function isInViewport(sectionID) {
-    const bounding = sectionID.getBoundingClientRect();
-    return (
-        bounding.top >= 0 &&
-        bounding.left >= 0 &&
-        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+    };
 }
 
 // Smooth scroll to anchor ID
@@ -66,10 +20,43 @@ function smoothScrollTo(anchorID) {
     });
 }
 
+function inViewport(section) {
+    const sectionOffset = Math.floor(section.getBoundingClientRect().top);
+    console.log("Line 25: " + sectionOffset);
+    if (sectionOffset < 150 && sectionOffset >= -150) {
+        console.log("Line 27");
+        return true;
+    } else {
+        return false;
+    };
+}
+
+function clearActive(section) {
+    console.log("clearActive running");
+    section.classList.remove("menu__link__active");
+}
+
+function isActive(isTrue, section) {
+    console.log(section);
+    if (isTrue) {
+        console.log("isActive running");
+        section.classList.add("menu__link__active");
+    };
+}
+
+// Find location of current section
+function activeSection() {
+    for (let i = 0; i < sections.length; i++) {
+        clearActive(sections[i]);
+        isActive(inViewport(sections[i]), sections[i]);
+    };
+}
+
 // MAIN EVENTS
 // Build menu 
 document.addEventListener("DOMContentLoaded", function() {
     buildNavbar(sections);
+    window.addEventListener("scroll", activeSection);
 
     // Scroll to section on link click
     navbarList.addEventListener("click", function(e) {
@@ -78,15 +65,7 @@ document.addEventListener("DOMContentLoaded", function() {
             e.preventDefault();
             // get clicked link
             const sectionToView = e.target.getAttribute("href");
-            console.log(sectionToView);
-
             smoothScrollTo(sectionToView);
-        }
-    });
-
-    // Set sections as active
-    //ADDED 6/20------------------------------
-    window.addEventListener("scroll", function() {
-        setSectionInViewActive(sections);
+        };
     });
 });
